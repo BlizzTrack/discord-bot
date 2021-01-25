c91f21d2331188237b31481576fdba66983e6045
Ruin <toyzz@live.com>
2021-01-25 17:31:09 +0000

    Fixed URLS for the new API endpoint design

--- Constants.ts
+++ Constants.ts
@@ -10,7 +10,7 @@ export const USE_BETA_URL = process.env.USE_BETA_URL == 'true';
 // Check Environment variables to determine which url to use.
 export const API_BASE = API_USE_BETA_URL ? BETA_BASE_URL : BASE_URL; // Used to make API calls.
 
-export const API_PATH = '/api/NGPD';
+export const API_PATH = '/api';
 
 export const MANIFEST_STABLE = (game: string, flag: string, seqn: number) => `${BASE_URL}/v/${game}/${flag}?latest-seqn=${seqn}`;
 export const MANIFEST_BETA = (game: string, flag: string, seqn: number) => `${BETA_BASE_URL}/v/${game}/${flag}?latest-seqn=${seqn}`;
@@ -20,8 +20,8 @@ export const MANIFEST = USE_BETA_URL ? MANIFEST_BETA : MANIFEST_STABLE;// (game:
 export const DOCS = `https://beta.blizztrack.com/Docs/Bot`; // TODO: Check for beta url once stable is updated.
 
 export const SUMMARY = `${API_BASE}${API_PATH}/summary`;
-export const VERSIONS = (game: string) => `${API_BASE}${API_PATH}/${game}/versions`;
-export const CDN = (game: string) => `${API_BASE}${API_PATH}/${game}/cdn`;
+export const VERSIONS = (game: string) => `${API_BASE}${API_PATH}/ngpd/${game}/versions`;
+export const CDN = (game: string) => `${API_BASE}${API_PATH}/ngpd/${game}/cdn`;
 
 // TODO: Come up with more aliases
 export const ALIASES: { [short: string]: string } = {
